bed7129b1479d902804bc29cb6978f7251822a10
Parker Bordeaux <pbordeaux@belltec.com>
2019-11-25 14:03:59 -0600

    bubble sort

---
+++ Algor/Sort/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algor.Sort
+{
+    public static partial class Sort
+    {
+        public static void BubbleSort<T>(ref IList<T> pIList) where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            bool rSwap = true;
+
+            while(rSwap)
+            {
+                rSwap = false;
+
+                for (int i = 0; i < pIList.Count-1; i++)
+                {
+                    if (pIList[i].CompareTo(pIList[i+1]) > 0)
+                    {
+                        _swap(ref pIList, i, i + 1);
+                        rSwap = true;
+                    }
+                }
+            }
+        }
+    }
+}
